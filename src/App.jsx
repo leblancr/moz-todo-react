b8997526ecd29963531555a2aab515d22aca3ac5
leblancr <rich.leblanc1@proton.me>
2024-09-06 23:16:20 -0400

    Finished delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,32 @@ function App(props) {
   // props = DATA from main.jsx
   // <App tasks={DATA} />
   // console.log(props)
-  const [tasks, setTasks] = useState(props.tasks);
-  
+  const [tasks, setTasks] = useState(props.tasks);  // list state variable
+
+  // Here, we define an updatedTasks constant that maps over the
+  // original tasks array. If the task's id property matches the
+  // id provided to the function, we use object spread syntax to
+  // create a new object, and toggle the completed property of
+  // that object before returning it. If it doesn't match,
+  // we return the original object.
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    }); // map ends here
+    setTasks(updatedTasks);  // new list with one item toggled
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   // map takes an array and returns a different array
   // (task) => task.name this is a function
   // map each task to a function that returns Todo object
@@ -18,6 +42,8 @@ function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
     )
   );
@@ -25,14 +51,10 @@ function App(props) {
   function addTask(name) {
     console.log('tasks', tasks)
     const lastItem = tasks[tasks.length - 1];
-    const lastId = tasks[tasks.length - 1]['id'];
-   
     console.log('lastItem', lastItem)
-    console.log('lastId', lastId)
-    
-    const newTask = { id: lastId + 1, name, completed: false };
+    const newTask = { id: lastItem['id'] + 1, name, completed: false };
     setTasks([...tasks, newTask]);  // spread syntax
-  }  
+  }
 
   // map instead of for loop
   const nums = [1, 2, 3, 4]
@@ -47,7 +69,7 @@ function App(props) {
   // console.log(sqrOfSqr)
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
-  const headingText = `${taskList.length} ${tasksNoun} remaining`;  
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -63,11 +85,10 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        {taskList}  
+        {taskList}
       </ul>
     </div>
   );
 }
 
 export default App;
-  
